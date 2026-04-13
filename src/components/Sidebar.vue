d549591f499c73f257ee460c641d1edd10d4eab8
Jax Tobyas <153058960+JaxT05@users.noreply.github.com>
2026-04-13 09:50:21 -0400

    init homepage and sidebar

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -12,15 +12,32 @@
 </template>
 <style scoped>
 nav {
+    position: absolute;
+    right: 0;
+    width: fit-content;
+    height: 100%;
     background-color: #99AD7A;
     display: flex;
     flex-direction: column;
     gap: 1rem;
+    padding: 2rem;
+    box-shadow: 2px 2px 8px var(--text-dark);
 }
 nav h2 { 
     color: var(--text-light);
+    text-align: left;
 }
 nav a {
     text-decoration: none;
+    border-bottom: 2px solid var(--text-light);
+    padding: 1rem;
+
+}
+nav a:last-child{
+    border: none;
+}
+
+nav a:active{
+    color: var(--text-dark);
 }
 </style>
